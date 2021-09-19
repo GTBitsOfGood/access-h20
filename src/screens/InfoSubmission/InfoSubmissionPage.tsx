cf3bd4810128085dde849caff1f9b865f797f3f1
Sukhmai Kapur <sukhmai.kapur@gmail.com>
2021-09-19 11:30:30 -0400

    added first two checkboxes

--- src/screens/InfoSubmission/InfoSubmissionPage.tsx
+++ src/screens/InfoSubmission/InfoSubmissionPage.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 function InfoSubmissionPage() {
   return (
-    <div>
-      <h1>Info Submission Page</h1>
-    </div>
+    <FormGroup>
+      <FormControlLabel control={<Checkbox />} label="Has the client made a minimum of 3 payments over the last 12 months?" />
+      <FormControlLabel control={<Checkbox />} label="Has the client contacted you in the past few months?" />
+    </FormGroup>
   );
 }
 
