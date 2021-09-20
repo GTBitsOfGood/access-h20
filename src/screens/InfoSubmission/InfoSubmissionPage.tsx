bf31ed12316ad7827c9a83f120a8bd316d3b3bff
ndasari6 <ndasari6@gatech.edu>
2021-09-20 02:43:29 -0400

    adding checkboxes and styling

--- src/screens/InfoSubmission/InfoSubmissionPage.tsx
+++ src/screens/InfoSubmission/InfoSubmissionPage.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Checkbox from "@material-ui/core/Checkbox";
+import TextField from "@material-ui/core/TextField"
 import FormGroup from "@material-ui/core/FormGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
+import {withStyles, createStyles} from "@material-ui/core/styles"
 
 function InfoSubmissionPage() {
   return (
     <FormGroup>
-      <FormControlLabel control={<Checkbox />} label="Has the client made a minimum of 3 payments over the last 12 months?" />
-      <FormControlLabel control={<Checkbox />} label="Has the client contacted you in the past few months?" />
+      <FormControlLabel control={<Checkbox />} label="Has the client made a minimum of 3 payments over the last 12 months?" labelPlacement="start" style={{justifyContent:'left'}}/>
+      <FormControlLabel control={<Checkbox />} label="Has the client contacted you in the past few months?" labelPlacement="start" style={{justifyContent:'left'}}/>
+      <FormControlLabel control={<Checkbox />} label="Does the client have another account in their name, thefts of service, anything to be aware of?" labelPlacement="start" style={{justifyContent:'left'}}/>
+      <FormControlLabel control={<TextField />} label="If so, then please elaborate." labelPlacement="start" style={{justifyContent:'left'}}/>
+      <FormControlLabel control={<Checkbox />} label="Are there signs of a leak that is not abated? " labelPlacement="start" style={{justifyContent:'left'}}/>
+      <FormControlLabel control={<TextField />} label="Has the client contacted you in the past few months?" labelPlacement="start" style={{justifyContent:'left'}}/>
     </FormGroup>
   );
 }
