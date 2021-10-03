e52d2bff563d9282d1c027ae399b98411eb826a1
Sukhmai Kapur <sukhmai.kapur@gmail.com>
2021-10-03 11:27:35 -0400

    added upload file button

--- src/screens/InfoSubmission/InfoSubmissionPage.tsx
+++ src/screens/InfoSubmission/InfoSubmissionPage.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Checkbox from "@material-ui/core/Checkbox";
 import TextField from "@material-ui/core/TextField"
 import FormGroup from "@material-ui/core/FormGroup";
+import Button from "@material-ui/core/Button";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import {withStyles, createStyles} from "@material-ui/core/styles"
 
 function InfoSubmissionPage() {
+  const [selectedFile, setSelectedFile] = useState();
+
+  const onChangeHandler = (e: React.ChangeEvent) => {
+    if (e && e.target) {
+      //@ts-ignore
+      setSelectedFile(e.target.files[0]) 
+    }
+    
+  }
+
   return (
     <FormGroup>
       <FormControlLabel control={<Checkbox />} label="Has the client made a minimum of 3 payments over the last 12 months?" labelPlacement="start" style={{justifyContent:'left'}}/>
@@ -14,6 +25,19 @@ function InfoSubmissionPage() {
       <FormControlLabel control={<TextField />} label="If so, then please elaborate." labelPlacement="start" style={{justifyContent:'left'}}/>
       <FormControlLabel control={<Checkbox />} label="Are there signs of a leak that is not abated? " labelPlacement="start" style={{justifyContent:'left'}}/>
       <FormControlLabel control={<TextField />} label="Additional comments/documentation?" labelPlacement="start" style={{justifyContent:'left'}}/>
+      <Button
+        variant="contained"
+        component="label"
+        style={{width:"400px"}}
+      >
+        Upload File:
+        <input
+          type="file"
+          name="file"
+          style={{padding:"5px"}}
+          onChange={(e) => onChangeHandler(e)}
+        />
+      </Button>
     </FormGroup>
   );
 }
