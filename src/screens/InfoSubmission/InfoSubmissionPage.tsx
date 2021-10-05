11d4555a23af6f0ffeb151834d188ae4784323c2
snara6 <46334243+sahyanara@users.noreply.github.com>
2021-10-05 01:54:34 -0400

    final styling

--- src/screens/InfoSubmission/InfoSubmissionPage.tsx
+++ src/screens/InfoSubmission/InfoSubmissionPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import { borderRadius } from "@mui/system";
 
 
 enum ApplicantStatus {  
@@ -135,93 +136,118 @@ function InfoSubmissionPage() {
       <hr style = {{width: "100%", border: "1px solid #DADADA"}}></hr>
 
 
-      <div style = {{display: "flex", width: "500px", height: "35px"}}>
-        <div style = {{flex: "150px"}}>
+      <div style = {{display: "flex", width: "1000px", height: "35px"}}>
+        <div style = {{flex: "1px"}}>
           <h3>Notes</h3>
         </div>
-        <div style = {{flex: "150px"}}>
+        <div style = {{flex: "550px"}}>
           <h3></h3>
           <a style = {{fontWeight: "normal", color: "#4DBAEA", textDecoration: "none", fontFamily:  "Roboto", padding: "15px"}}>+ Add Note</a>
         </div>
       </div>
 
-      <hr style = {{width: "100%", border: "1px solid #DADADA", marginTop: "30px", marginBottom: "30px"}}></hr>
+      <hr style = {{width: "100%", border: "1px solid #DADADA", marginTop: "30px", marginBottom: "20px"}}></hr>
 
-      <h3>Eligibility</h3>
-      <div>
-        <div style = {{display: "flex", width: "1000px"}}>
-          <div style = {{flex: "50px"}}>
-            <Button type="button" id = "yes1" onClick = {() => toggleButton(paymentAns, "yes1", "no1", true)}>Yes</Button>
-            <Button type="button" id = "no1" onClick = {() => toggleButton(paymentAns, "yes1", "no1", false)}>No</Button>
-          </div>
-          <div style = {{flex: "700px"}}>
-            <p style = {{fontWeight: "bold"}}>Payments</p>
-            <p style = {{fontWeight: "lighter"}}>Has the client made a minimum of 3 payments over the last 12 months?</p>
+      <div style = {{display: "flex", width: "1000px"}}>
+        <div style = {{flex: "50px"}}>
+          <h3>Eligibility</h3>
+        </div>
+        <div style = {{flex: "50px"}}>
+          <div style = {{paddingLeft: "150px"}}>
+            <div style = {{display: "flex", width: "1000px"}}>
+              <div style = {{flex: "50px"}}>
+                <Button type="button" id = "yes1" onClick = {() => toggleButton(paymentAns, "yes1", "no1", true)}>Yes</Button>
+                <Button type="button" id = "no1" onClick = {() => toggleButton(paymentAns, "yes1", "no1", false)}>No</Button>
+              </div>
+              <div style = {{flex: "700px"}}>
+                <p style = {{fontWeight: "normal", fontFamily:  "Roboto"}}>Payments</p>
+                <p style = {{fontWeight: "lighter", color: "#A1A1A1", fontFamily:  "Roboto"}}>Has the client made a minimum of 3 payments over the last 12 months?</p>
+              </div>
+            </div>
+            <div style = {{display: "flex", width: "1000px"}}>
+              <div style = {{flex: "50px"}}>
+                <Button type="button" id = "yes2" onClick = {() => toggleButton(servicesAns, "yes2", "no2", true)}>Yes</Button>
+                <Button type="button" id = "no2" onClick = {() => toggleButton(servicesAns, "yes2", "no2", false)}>No</Button>
+              </div>
+              <div style = {{flex: "700px"}}>
+                <p style = {{fontWeight: "normal", fontFamily:  "Roboto"}}>Minimum Services</p>
+                <p style = {{fontWeight: "lighter", color: "#A1A1A1", fontFamily:  "Roboto"}}>Does the customer have a minimum of 12 months of service?</p>
+              </div>
+            </div>
+            <div style = {{display: "flex", width: "1000px"}}>
+              <div style = {{flex: "50px"}}>
+                <Button type="button" id = "yes3" onClick = {() => toggleButton(contactAns, "yes3", "no3", true)}>Yes</Button>
+                <Button type="button" id = "no3"  onClick = {() => toggleButton(contactAns, "yes3", "no3", false)}>No</Button>
+              </div>
+              <div style = {{flex: "700px"}}>
+                <p style = {{fontWeight: "normal", fontFamily:  "Roboto"}}>Customer Contact</p>
+                <p style = {{fontWeight: "lighter", color: "#A1A1A1", fontFamily:  "Roboto"}}>Has the customer been in contact with your utility company?</p>
+              </div>
+            </div>
+            <div style = {{display: "flex", width: "1000px"}}>
+              <div style = {{flex: "50px"}}>
+                <Button type="button" id = "yes4" onClick = {() => toggleButton(waterAns, "yes4", "no4", true)}>Yes</Button>
+                <Button type="button" id = "no4" onClick = {() => toggleButton(waterAns, "yes4", "no4", false)}>No</Button>
+              </div>
+              <div style = {{flex: "700px"}}>
+                <p style = {{fontWeight: "normal", fontFamily:  "Roboto"}}>Water Meter</p>
+                <p style = {{fontWeight: "lighter", color: "#A1A1A1", fontFamily:  "Roboto"}}>Does the property with dedicated water meter?</p>
+              </div>
+            </div>
           </div>
         </div>
-        <div style = {{display: "flex", width: "1000px"}}>
-          <div style = {{flex: "50px"}}>
-            <Button type="button" id = "yes2" onClick = {() => toggleButton(servicesAns, "yes2", "no2", true)}>Yes</Button>
-            <Button type="button" id = "no2" onClick = {() => toggleButton(servicesAns, "yes2", "no2", false)}>No</Button>
-          </div>
-          <div style = {{flex: "700px"}}>
-            <p style = {{fontWeight: "bold"}}>Minimum Services</p>
-            <p style = {{fontWeight: "lighter"}}>Does the customer have a minimum of 12 months of service?</p>
-          </div>
-        </div>
-        <div style = {{display: "flex", width: "1000px"}}>
-          <div style = {{flex: "50px"}}>
-            <Button type="button" id = "yes3" onClick = {() => toggleButton(contactAns, "yes3", "no3", true)}>Yes</Button>
-            <Button type="button" id = "no3"  onClick = {() => toggleButton(contactAns, "yes3", "no3", false)}>No</Button>
-          </div>
-          <div style = {{flex: "700px"}}>
-            <p style = {{fontWeight: "bold"}}>Customer Contact</p>
-            <p style = {{fontWeight: "lighter"}}>Has the customer been in contact with your utility company?</p>
-          </div>
-        </div>
-        <div style = {{display: "flex", width: "1000px"}}>
-          <div style = {{flex: "50px"}}>
-            <Button type="button" id = "yes4" onClick = {() => toggleButton(waterAns, "yes4", "no4", true)}>Yes</Button>
-            <Button type="button" id = "no4" onClick = {() => toggleButton(waterAns, "yes4", "no4", false)}>No</Button>
-          </div>
-          <div style = {{flex: "700px"}}>
-            <p style = {{fontWeight: "bold"}}>Water Meter</p>
-            <p style = {{fontWeight: "lighter"}}>Does the property with dedicated water meter?</p>
-          </div>
-        </div>
-      </div>
-      <h3>Document Submission</h3>
-      <div style = {{display: "flex", width: "300px", justifyContent: "space-between"}}>
-        <Button  variant="contained" component="label" style = {{flex: "50px", width: "75px", height: "50px"}}>
-          Upload
-          <input id="paymentFile" type="file" hidden onChange = {() => saveFile("paymentFile",file1)}/>
-        </Button>
-        <p style = {{flex: "50px", padding: "10px"}}>Payment History</p>
-      </div>
-      <div style = {{display: "flex", width: "300px", justifyContent: "space-between"}}>
-        <Button id="usageFile" variant="contained" component="label" style = {{flex: "50px", width: "75px", height: "50px"}}>
-          Upload
-          <input id="usageFile" type="file" hidden onChange = {() => saveFile("usageFile",file2)}/>
-        </Button>
-        <p style = {{flex: "50px", padding: "10px"}}>Usage History</p>
       </div>
 
-      <h3>Other</h3>
-      <div>
-        <p>Are there any pending adjustments?</p>
-        <textarea id="adjustAns" type= "text" style= {{width: "75%", height: "150px"}} onChange = {() => setVar("adjustAns", adjustAns)}></textarea>
+      <hr style = {{width: "100%", border: "1px solid #DADADA", marginTop: "30px", marginBottom: "20px"}}></hr>
+      
+      <div style = {{display: "flex", width: "1000px"}}>
+        <div style = {{flex: "1px"}}>
+          <h3>Document Submission</h3>
+        </div>
+        <div style = {{flex: "500px"}}>
+          <div>
+            <p>Payment History</p>
+            <p style = {{fontWeight: "lighter", color: "#A1A1A1", fontFamily:  "Roboto"}}>Please upload the customers' bill payment history over the last 12 months.</p>
+            <Button  variant="contained" component="label" style = {{flex: "50px", width: "126px", height: "40px", borderRadius: "10px"}}>
+              Upload
+              <input id="paymentFile" type="file" hidden onChange = {() => saveFile("paymentFile",file1)}/>
+            </Button>
+          </div>
+          <div>
+            <p>Usage History</p>
+            <p style = {{fontWeight: "lighter", color: "#A1A1A1", fontFamily:  "Roboto"}}>Please upload the customers' usage history over the last 12 months.</p>
+            <Button id="usageFile" variant="contained" component="label" style = {{flex: "50px", width: "126px", height: "40px", borderRadius: "10px"}}>
+              Upload
+              <input id="usageFile" type="file" hidden onChange = {() => saveFile("usageFile",file2)}/>
+            </Button>
+          </div>
+        </div>
       </div>
-      <div>
-        <p>What (if any) other individuals are involved (spouse, landlord, dependent)?</p>
-        <TextField id="indivAns" onChange = {() => setVar("indivAns", indivAns)}></TextField>
+
+      <hr style = {{width: "100%", border: "1px solid #DADADA", marginTop: "30px", marginBottom: "20px"}}></hr>
+
+      <div style = {{display: "flex", width: "1000px"}}>
+        <div style = {{flex: "1px"}}>
+          <h3>Questions</h3>
+        </div>
+        <div style = {{flex: "500px"}}>
+          <div>
+            <p>Are there any pending adjustments?</p>
+            <textarea id="adjustAns" type= "text" style= {{width: "75%", height: "150px", borderRadius: "10px"}} onChange = {() => setVar("adjustAns", adjustAns)}></textarea>
+          </div>
+          <div>
+            <p>What (if any) other individuals are involved (spouse, landlord, dependent)?</p>
+            <textarea id="indivAns" type= "text" style= {{width: "75%", height: "150px", borderRadius: "10px"}} onChange = {() => setVar("indivAns", indivAns)}></textarea>
+          </div>
+          <div>
+            <p>Is there any additional information we should know about the account?</p>
+            <textarea id="infoAns" type= "text" style= {{width: "75%", height: "150px", borderRadius: "10px"}} onChange = {() => setVar("infoAns", infoAns)}></textarea>
+          </div>
+          <Button type="button" style = {{backgroundColor: "#4DBAEA", color: "#FFFFFF", borderRadius: "8px", height: "40px", width: "125px"}} onClick = {(() => console.log(information))}>
+              Save
+          </Button>
+        </div>
       </div>
-      <div>
-        <p>Is there any additional information we should know about the account?</p>
-        <textarea id="infoAns" type= "text" style= {{width: "75%", height: "150px"}} onChange = {() => setVar("infoAns", infoAns)}></textarea>
-      </div>
-      <Button type="button" onClick = {(() => console.log(information))}>
-          Save
-      </Button>
     </div>
 
   )
