54bed3799b1fb9a284a941cbef96a46b69db9a5b
Ila Sharma <ilasharma@lawn-143-215-125-233.lawn.gatech.edu>
2021-11-02 00:39:50 -0400

    added applicant nav bar

--- src/screens/ApplicantView/ApplicantViewPage.tsx
+++ src/screens/ApplicantView/ApplicantViewPage.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import ApplicantTable from "../../components/ApplicantTable";
 import classes from "./ApplicantView.module.css";
 import urls from "../../../utils/urls";
+import ApplicantNavLink from "../../components/ApplicantNavLink"
 
 function ApplicantViewPage() {
   return (
     <>
+      <ApplicantNavLink />
       <h1 className={classes.header}>Dashboard</h1>
       <ApplicantTable
         isUtilityView={false}
