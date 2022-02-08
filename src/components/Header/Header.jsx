1f157c54dc71e7c64dbe753fd9a5d9b6ddc53ee3
Charlie Luo <cvxluo@gmail.com>
2022-02-08 01:55:35 -0500

    Small bugfix from deleting imports

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx'
 import PropTypes from 'prop-types'
+import NavLink from '../NavLink' // eslint-disable-line
 import routes from './routes'
 import styles from './Header.module.css'
 
