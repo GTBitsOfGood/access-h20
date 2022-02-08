942afee12fa2edb14ef57d02eaf666a34a4f4269
luke9kim8 <luke9kim8@gmail.com>
2022-02-08 04:52:40 -0500

    Add styles

--- src/screens/InfoSubmission/InfoSubmissionPage.tsx
+++ src/screens/InfoSubmission/InfoSubmissionPage.tsx
@@ -2,12 +2,11 @@ import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import { ApplicantStatus, ApplicantStatusColor } from "../../types/Applicant";
-import { Checkbox } from "@mui/material";
-import Menu from "@mui/material/Menu";
+import { Checkbox, Select } from "@mui/material";
 import MenuItem from "@mui/material/MenuItem";
-import Select from "@mui/material/Select";
-import FormControl from "@mui/material/FormControl";
+import Menu from "@mui/material/Menu";
 import style from "./InfoSubmission.module.css";
+import { FormControl } from "@material-ui/core";
 
 interface Applicant {
   name: string;
@@ -109,36 +108,31 @@ const InfoSubmissionPage = ({ applicantId }: PropTypes): JSX.Element => {
         </a>
         <h1>{dummyData.name}</h1>
         <div className={style.customerInfo}>
-          <div className={style.customerComponentList}>
-            <div className={style.customerComponent}>
-              <h4>Status</h4>
-              <Select
-                id="status-menu"
-                onChange={handleStatusClick}
-                value={status}
-              >
-                <MenuItem value={"Awaiting Utility"}>Awaiting Utility</MenuItem>
-                <MenuItem value={"Terminated"}>Terminated</MenuItem>
-                <MenuItem value={"Denied"}>Denied</MenuItem>
-                <MenuItem value={"Completed"}>Completed</MenuItem>
-                <MenuItem value={"Approved"}>Approved</MenuItem>
-                <MenuItem value={"Awaiting AccessH2O"}>
-                  Awaiting AccessH2O
-                </MenuItem>
-              </Select>
-            </div>
-            <div className={style.customerComponent}>
-              <h5>Account ID</h5>
-              <h4>{dummyData.accountId}</h4>
-            </div>
-            <div className={style.customerComponent}>
-              <h4>Phone Number</h4>
-              <h4>{dummyData.phoneNumber}</h4>
-            </div>
-            <div className={style.customerComponent}>
-              <h4>Address</h4>
-              <h4>{dummyData.propertyAddress}</h4>
-            </div>
+          <div className={style.customerComponent}>
+            <h4>Status</h4>
+
+            <Select id="status-menu" onChange={handleStatusClick}>
+              <MenuItem value={"Awaiting Utility"}>Awaiting Utility</MenuItem>
+              <MenuItem value={"Terminated"}>Terminated</MenuItem>
+              <MenuItem value={"Denied"}>Denied</MenuItem>
+              <MenuItem value={"Completed"}>Completed</MenuItem>
+              <MenuItem value={"Approved"}>Approved</MenuItem>
+              <MenuItem value={"Awaiting AccessH2O"}>
+                Awaiting AccessH2O
+              </MenuItem>
+            </Select>
+          </div>
+          <div className={style.customerComponent}>
+            <h4>Account ID</h4>
+            <h4>{dummyData.accountId}</h4>
+          </div>
+          <div className={style.customerComponent}>
+            <h4>Phone Number</h4>
+            <h4>{dummyData.phoneNumber}</h4>
+          </div>
+          <div className={style.customerComponent}>
+            <h4>Address</h4>
+            <h4>{dummyData.propertyAddress}</h4>
           </div>
         </div>
 
@@ -149,37 +143,29 @@ const InfoSubmissionPage = ({ applicantId }: PropTypes): JSX.Element => {
             <h3>Notes</h3>
           </div>
 
-          <div className={style.sectionNotesContainer}>
-            <div className={style.noteStack}>
-              {notes.map((note) => (
-                <TextField
-                  id="savedNotes"
-                  variant="filled"
-                  defaultValue={note}
-                  className={style.stickyNote}
-                  InputProps={{ readOnly: true, disableUnderline: true }}
-                ></TextField>
+          <div className={style.sectionContentContainer}>
+            {notes.map((note) => (
+                <div className={style.stickyNote}>{note}</div>
               ))}
-            </div>
-
-            <div>
-              {textFieldDisplayed ? (
+            {textFieldDisplayed ? (
+              <FormControl fullWidth>
                 <TextField
                   id="notesField"
                   label="Add your note here"
                   variant="outlined"
                   onChange={(e) => setCurrentInput(e.target.value)}
                   value={currentInput}
-                ></TextField>
-              ) : (
-                <a onClick={() => setTextFieldDisplayed(true)}> + Add Note </a>
-              )}
-            </div>
+                />
+              </FormControl>
+            ) : (
+              <a onClick={() => setTextFieldDisplayed(true)}> + Add Note </a>
+            )}
+
             <div>
               <Button
                 id="note"
                 onClick={() => {
-                  if (textFieldDisplayed) {
+                  if (textFieldDisplayed && currentInput) {
                     setNotes([...notes, currentInput]);
                     setCurrentInput("");
                     setTextFieldDisplayed(false);
@@ -207,19 +193,18 @@ const InfoSubmissionPage = ({ applicantId }: PropTypes): JSX.Element => {
         </div>
 
         <hr />
+
         <div className={style.sectionContainer}>
           <div className={style.sectionTitleContainer}>
             <h3>Eligibility</h3>
           </div>
 
-          <div className={style.sectionEligibilityContainer}>
-            <div className={style.eligibilityStack}>
-              <div>
-                <Checkbox
-                  checked={paymentAns}
-                  onChange={() => setPaymentAns(!paymentAns)}
-                />
-              </div>
+          <div className={style.sectionContentContainer}>
+            <div className={style.eligibility}>
+              <Checkbox
+                checked={paymentAns}
+                onChange={() => setPaymentAns(!paymentAns)}
+              />
               <div>
                 <p>Payments</p>
                 <p>
@@ -227,40 +212,44 @@ const InfoSubmissionPage = ({ applicantId }: PropTypes): JSX.Element => {
                   months?
                 </p>
               </div>
-              <div>
-                <Checkbox
-                  checked={servicesAns}
-                  onChange={() => setServicesAns(!servicesAns)}
-                />
-              </div>
+            </div>
+
+            <div className={style.eligibility}>
+              <Checkbox
+                checked={servicesAns}
+                onChange={() => setServicesAns(!servicesAns)}
+              />
               <div>
                 <p>Minimum Services</p>
                 <p>Does the customer have a minimum of 12 months of service?</p>
               </div>
-              <div>
-                <Checkbox
-                  checked={contactAns}
-                  onChange={() => setContactAns(!contactAns)}
-                />
-              </div>
+            </div>
+
+            <div className={style.eligibility}>
+              <Checkbox
+                checked={contactAns}
+                onChange={() => setContactAns(!contactAns)}
+              />
               <div>
                 <p>Customer Contact</p>
                 <p>
                   Has the customer been in contact with your utility company?
                 </p>
               </div>
-              <div>
-                <Checkbox
-                  checked={waterAns}
-                  onChange={() => setWaterAns(!waterAns)}
-                />
-              </div>
+            </div>
+
+            <div className={style.eligibility}>
+              <Checkbox
+                checked={waterAns}
+                onChange={() => setWaterAns(!waterAns)}
+              />
               <div>
                 <p>Water Meter</p>
                 <p>Does the property with dedicated water meter?</p>
               </div>
             </div>
           </div>
+          <div className={style.sectionRightFiller}></div>
         </div>
 
         <hr />
@@ -270,83 +259,83 @@ const InfoSubmissionPage = ({ applicantId }: PropTypes): JSX.Element => {
             <h3>Documents</h3>
           </div>
 
-          <div className={style.sectionDocumentsContainer}>
-            <div className={style.documentStack}>
-              <div>
-                <Button variant="contained" component="label">
-                  Upload
-                  <input
-                    id="paymentFile"
-                    type="file"
-                    hidden
-                    onChange={(e) => {
-                      if (
-                        e.target.files === null ||
-                        e.target.files.length < 1
-                      ) {
-                        alert("Please upload a valid file.");
-                        return;
-                      }
+          <div className={style.sectionContentContainer}>
+            <div>
+              <Button variant="contained" component="label">
+                Upload
+                <input
+                  id="paymentFile"
+                  type="file"
+                  hidden
+                  onChange={(e) => {
+                    if (e.target.files === null || e.target.files.length < 1) {
+                      alert("Please upload a valid file.");
+                      return;
+                    }
 
-                      setPaymentFile(e.target.files[0]);
-                    }}
-                  />
-                </Button>
-                <p>Payment History</p>
-              </div>
+                    setPaymentFile(e.target.files[0]);
+                  }}
+                />
+              </Button>
+              <p>Payment History</p>
+            </div>
 
-              <div>
-                <Button id="usageFile" variant="contained" component="label">
-                  Upload
-                  <input
-                    id="paymentFile"
-                    type="file"
-                    hidden
-                    onChange={(e) => {
-                      if (
-                        e.target.files === null ||
-                        e.target.files.length < 1
-                      ) {
-                        alert("Please upload a valid file.");
-                        return;
-                      }
+            <div>
+              <Button id="usageFile" variant="contained" component="label">
+                Upload
+                <input
+                  id="paymentFile"
+                  type="file"
+                  hidden
+                  onChange={(e) => {
+                    if (e.target.files === null || e.target.files.length < 1) {
+                      alert("Please upload a valid file.");
+                      return;
+                    }
 
-                      setUsageFile(e.target.files[0]);
-                    }}
-                  />
-                </Button>
-                <p>Usage History</p>
-              </div>
+                    setUsageFile(e.target.files[0]);
+                  }}
+                />
+              </Button>
+              <p>Usage History</p>
             </div>
           </div>
+          <div className={style.sectionRightFiller}></div>
         </div>
 
         <hr />
 
-        <h3>Additional</h3>
-        <div>
-          <p>Are there any pending adjustments?</p>
-          <TextField id="notesField" variant="outlined"></TextField>
+        <div className={style.sectionContainer}>
+          <div className={style.sectionTitleContainer}>
+            <h3>Additional</h3>
+          </div>
+          <div className={style.sectionContentContainer}>
+            <div>
+              <p>Are there any pending adjustments?</p>
+              <TextField id="notesField" />
+              <p>
+                What (if any) other individuals are involved (spouse, landlord,
+                dependent)?
+              </p>
+              <TextField
+                id="indivAns"
+                onChange={(e) => setIndivAns(e.target.value)}
+              />
+            </div>
+            <div>
+              <p>
+                Is there any additional information we should know about the
+                account?
+              </p>
+              <TextField
+                id="infoAns"
+                onChange={(e) => setInfoAns(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className={style.sectionRightFiller}></div>
+        </div>
 
-          <p>
-            What (if any) other individuals are involved (spouse, landlord,
-            dependent)?
-          </p>
-          <TextField
-            id="indivAns"
-            onChange={(e) => setIndivAns(e.target.value)}
-          />
-        </div>
-        <div>
-          <p>
-            Is there any additional information we should know about the
-            account?
-          </p>
-          <TextField
-            id="infoAns"
-            onChange={(e) => setInfoAns(e.target.value)}
-          />
-        </div>
         <Button
           type="button"
           onClick={() => console.log(generateInfoSubmission())}
