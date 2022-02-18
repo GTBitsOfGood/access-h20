0f727c4c44d89c1cb943700d97e426d47d822d5a
Ila Sharma <ilasharma@lawn-143-215-118-251.lawn.gatech.edu>
2022-02-18 11:18:54 -0500

    fix all remaining lint issues

--- src/screens/UtilityView/ApplicantView/ApplicantViewPage.tsx
+++ src/screens/UtilityView/ApplicantView/ApplicantViewPage.tsx
@@ -7,7 +7,7 @@ import ApplicantNavLink from '../../../components/ApplicantNavLink'
 const ApplicantViewPage = (): JSX.Element => {
   return (
     <>
-      <ApplicantNavLink 
+      <ApplicantNavLink
         isUtilityView={true}
       />
       <h1 className={classes.header}>Dashboard</h1>
