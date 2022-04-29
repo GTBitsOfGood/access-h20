4a7a572c1ccbca9b323fda7fc95b8da4100070d6
cvxluo <cvxluo@gmail.com>
2022-04-29 08:13:53 -0400

    Testing removing redirects

--- next.config.js
+++ next.config.js
@@ -12,14 +12,5 @@ module.exports = withImages({
       MONGO_DB: process.env.MONGO_DB,
       JWT_SECRET: process.env.JWT_SECRET
     }
-  },
-  async redirects () {
-    return [
-      {
-        source: '/',
-        destination: '/login',
-        permanent: true
-      }
-    ]
   }
 })
